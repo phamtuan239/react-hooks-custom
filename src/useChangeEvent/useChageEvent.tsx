aa0a9f59411af431e7e5ede94e0b65f1fd29875f
minggu99 <tuanphamtran99@gmail.com>
2022-06-25 22:46:04 +0700

    update code logic useChangeEvent

--- src/useChangeEvent/useChageEvent.tsx
+++ src/useChangeEvent/useChageEvent.tsx
@@ -15,17 +15,26 @@ export type OnChangeFunc<E extends EventInput = any> = (
 
 export type FilterType<F> = F extends string ? string : F extends number ? number : F;
 
-const useChangeEvent = <T extends Input>(input: FilterType<T>) => {
+export type UseChangeEventReturn<T> = [
+  value: FilterType<T>,
+  setValue: React.Dispatch<React.SetStateAction<FilterType<T>>>,
+  onChange: OnChangeFunc<any>,
+  convertValue?: () => void
+];
+
+const useChangeEvent = <T extends Input>(
+  input: FilterType<T>
+): UseChangeEventReturn<T> => {
   const [value, setValue] = useState<FilterType<T>>(input);
 
   const onChangePrimitive: OnChangeFunc = (e) => {
-    const inputValue = convertInputForm(e.target.value);
+    const inputValue = e.target.value;
     setValue(inputValue);
   };
 
   const onChangeObject: OnChangeFunc = (e) => {
     setValue((pre: any) => {
-      return { ...pre, [e.target.name]: convertInputForm(e.target.value) };
+      return { ...pre, [e.target.name]: e.target.value };
     });
   };
 
@@ -34,7 +43,22 @@ const useChangeEvent = <T extends Input>(input: FilterType<T>) => {
       ? onChangePrimitive
       : onChangeObject;
 
-  return { value, onChange };
+  const convertValue = () => {
+    if (typeof value === "string" || typeof value === "number") {
+      setValue(convertInputForm(value as string));
+    } else {
+      for (const property in value) {
+        setValue((pre: any) => {
+          return {
+            ...pre,
+            [property]: convertInputForm(value[property] + ""),
+          };
+        });
+      }
+    }
+  };
+
+  return [value, setValue, onChange, convertValue];
 };
 
 export default useChangeEvent;
